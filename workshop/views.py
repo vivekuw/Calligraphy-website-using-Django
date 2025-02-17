1f43afe7da841878e6b9a972e676dd08792a41b7
Vivek Wadher <122199360+vivekuw@users.noreply.github.com>
2025-02-17 23:23:04 +0530

    Update views.py

--- workshop/views.py
+++ workshop/views.py
@@ -19,30 +19,3 @@ def workshop(request):
     work = Workshop.objects.all()
     return render(request,'workshop.html' , {'work':work})
 
-def login(request):
-    def user_login(request):
-        if request.method == 'POST':
-            email = request.POST['email']
-            password = request.POST['password']
-
-            # Get user by email
-            try:
-                user = User.objects.get(email=email)
-                user = authenticate(request, username=user.username, password=password)
-            except User.DoesNotExist:
-                user = None
-
-            if user is not None:
-                login(request, user)
-                return redirect('/home')  # Redirect to profile or dashboard
-            else:
-                msg = 'Invalid email or password'
-                form = AuthenticationForm(request.POST)
-                return render(request, 'login.html', {'form': form, 'msg': msg})
-        else:
-            form = AuthenticationForm()
-        return render(request, 'login.html', {'form': form } )
-
-
-def register(request):
-    return render(request,'login.html')
